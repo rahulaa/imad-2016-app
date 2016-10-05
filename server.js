54930d9069d698f63ec7e31962d2cddc0985050a
rahulaa <ahujarahul000@gmail.com>
2016-10-05 19:38:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-1',function(req,res){
-res.send('article 1 will be served here')    
-}};
+res.send('article 1 will be served here');    
+});
 app.get('/article-2',function(req,res){
-res.send('article 2 will be served here')    
-}};
+res.send('article 2 will be served here') ;   
+});
 app.get('/article-3',function(req,res){
-res.send('article 3 will be served here')    
-}};
+res.send('article 3 will be served here')  ;  
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
